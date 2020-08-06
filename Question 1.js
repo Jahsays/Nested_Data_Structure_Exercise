4f60f24a6699fb0fa258d1ef8368e1f5b1dcc1bd
Chiaka Williams <Chiaka394@gmail.com>
2020-08-06 11:21:59 +0100

    Adding the first question

---
+++ Question 1.js
@@ -0,0 +1,19 @@
+// var primeNumbersArray = nestedData.innerData.numberData.primeNumbers;
+// for(var i=0; i<primeNumbersArray.length; i++){
+//   console.log(primeNumbersArray[i])
+
+
+// var fibonacciArray = nestedData.innerData.numberData.fibonnaci;
+// for(var i=0; i<fibonacciArray.length;i++){
+//   if(fibonacciArray[i] % 2===0){
+//   console.log(fibonacciArray[i])
+//  }
+// } 
+
+
+// nestedData.innerData.order[1]
+
+// nestedData.innerData.addSnack("chocolate")
+
+
+// "this" refers to the global Object (not like I fully grasp)
