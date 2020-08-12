6172b829588d00bab68fab4b662192b8e1f7244a
Chiaka Williams <Chiaka394@gmail.com>
2020-08-12 16:36:23 +0100

    Added the questions to file one and two. Added a new filr for  question three

--- Question 1.js
+++ Question 1.js
@@ -1,3 +1,26 @@
+// 1.	Given the following nested object:
+// var nestedData = {
+//   innerData: {
+//     order: ["first", "second", "third"],
+//     snacks: ["chips", "fruit", "crackers"],
+//     numberData: {
+//         primeNumbers: [2,3,5,7,11],
+//         fibonnaci: [1,1,2,3,5,8,13]
+//     },
+//     addSnack: function(snack){
+//         this.snacks.push(snack);
+//         return this;
+//     }
+//   }
+// }
+// Using a for loop, console.log all of the numbers in the primeNumbers array.
+// Using a for loop, console.log all of the even Fibonnaci numbers.
+// Console.log the value "second" inside the order array.
+// Invoke the addSnack function and add the snack "chocolate".
+// Inside of the addSnack function there is a special keyword called this. What does the word this refer to inside the addSnack function?
+
+
+
 // var primeNumbersArray = nestedData.innerData.numberData.primeNumbers;
 // for(var i=0; i<primeNumbersArray.length; i++){
 //   console.log(primeNumbersArray[i])
