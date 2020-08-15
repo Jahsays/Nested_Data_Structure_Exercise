d43b9d17a772f4a52309bdc533cae0ae16f78893
Chiaka Williams <Chiaka394@gmail.com>
2020-08-15 22:56:56 +0100

    Uncommented previously commented code blocks

--- Question 1.js
+++ Question 1.js
@@ -1,42 +1,42 @@
-// 1.	Given the following nested object:
-// var nestedData = {
-//   innerData: {
-//     order: ["first", "second", "third"],
-//     snacks: ["chips", "fruit", "crackers"],
-//     numberData: {
-//         primeNumbers: [2,3,5,7,11],
-//         fibonnaci: [1,1,2,3,5,8,13]
-//     },
-//     addSnack: function(snack){
-//         this.snacks.push(snack);
-//         return this;
-//     }
-//   }
-// }
-// Using a for loop, console.log all of the numbers in the primeNumbers array.
-// Using a for loop, console.log all of the even Fibonnaci numbers.
-// Console.log the value "second" inside the order array.
-// Invoke the addSnack function and add the snack "chocolate".
-// Inside of the addSnack function there is a special keyword called this. What does the word this refer to inside the addSnack function?
+1.	Given the following nested object:
+var nestedData = {
+  innerData: {
+    order: ["first", "second", "third"],
+    snacks: ["chips", "fruit", "crackers"],
+    numberData: {
+        primeNumbers: [2,3,5,7,11],
+        fibonnaci: [1,1,2,3,5,8,13]
+    },
+    addSnack: function(snack){
+        this.snacks.push(snack);
+        return this;
+    }
+  }
+}
+Using a for loop, console.log all of the numbers in the primeNumbers array.
+Using a for loop, console.log all of the even Fibonnaci numbers.
+Console.log the value "second" inside the order array.
+Invoke the addSnack function and add the snack "chocolate".
+Inside of the addSnack function there is a special keyword called this. What does the word this refer to inside the addSnack function?
 
 
 
-// var primeNumbersArray = nestedData.innerData.numberData.primeNumbers;
-// for(var i=0; i<primeNumbersArray.length; i++){
-//   console.log(primeNumbersArray[i])
+var primeNumbersArray = nestedData.innerData.numberData.primeNumbers;
+for(var i=0; i<primeNumbersArray.length; i++){
+  console.log(primeNumbersArray[i])
 
 
-// var fibonacciArray = nestedData.innerData.numberData.fibonnaci;
-// for(var i=0; i<fibonacciArray.length;i++){
-//   if(fibonacciArray[i] % 2===0){
-//   console.log(fibonacciArray[i])
-//  }
-// } 
+var fibonacciArray = nestedData.innerData.numberData.fibonnaci;
+for(var i=0; i<fibonacciArray.length;i++){
+  if(fibonacciArray[i] % 2===0){
+  console.log(fibonacciArray[i])
+ }
+} 
 
 
-// nestedData.innerData.order[1]
+nestedData.innerData.order[1]
 
-// nestedData.innerData.addSnack("chocolate")
+nestedData.innerData.addSnack("chocolate")
 
 
-// "this" refers to the global Object (not like I fully grasp)
+"this" refers to the global Object (not like I fully grasp)
--- Question 4.js
+++ Question 4.js
@@ -1,4 +1,4 @@
-// 4.	Write a function called makeXOGrid which takes in two parameters, rows and columns, and returns an array of arrays with the number of values in each subarray equal to the columns parameter and the number of subarrays equal to the rows parameter. The values in the sub-arrays should switch between "X" and "O".
+4.	Write a function called makeXOGrid which takes in two parameters, rows and columns, and returns an array of arrays with the number of values in each subarray equal to the columns parameter and the number of subarrays equal to the rows parameter. The values in the sub-arrays should switch between "X" and "O".
 
 
 
